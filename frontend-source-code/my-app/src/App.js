49cb02eb93b478cb15e091adbe3fcd4879582712
BasantSamy22 <basantsamyaziz2013@gmail.com>
2023-12-05 16:35:29 +0200

    .

---
+++ frontend-source-code/my-app/src/App.js
@@ -0,0 +1,19 @@
+
+//import React ,{useState} from 'react';
+import Navbar from "./navbar";
+import { React } from 'react';
+
+
+function App(){
+
+return(
+     
+         <Navbar></Navbar>
+)
+;
+
+
+  
+
+}
+export default App;
---
+++ frontend-source-code/my-app/src/navbar.js
@@ -0,0 +1,28 @@
+const Navbar = () => {
+    return ( 
+      <nav className="navbar" >
+        <div className="links">
+            <a herf="/">HOME</a>
+            <a herf="/store">STORE</a>
+            <a herf="/contact">CONTACT</a>
+            <a herf="/about">ABOUT</a>
+ 
+        </div>
+        <div className="links2">
+          <a herf="/SignIn ">Sign In</a>
+          <a herf="/SignUp ">Sign Up</a>
+          <a herf="/profile">ME</a>
+
+
+        </div>
+        
+
+
+          
+      </nav>
+
+
+     );
+}
+ 
+export default Navbar;
