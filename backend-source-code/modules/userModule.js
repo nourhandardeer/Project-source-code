d57ef48d231c15140bd75fd9ebaa13722c9575f3
nourhandardeer <nourhandardeer3@gmail.com>
2023-12-05 17:14:45 +0200

    Update userModule.js

--- backend-source-code/modules/userModule.js
+++ backend-source-code/modules/userModule.js
@@ -12,5 +12,16 @@ const userSchema= mongoose.Schema({
     password : {
         type: String ,
         require: true
+    },
+    isAdmin : {
+        type: boolean ,
+        require: true ,
+        default: false
     }
-})
+},
+{
+    timestamps: true
+}
+)
+const User = mongoose.model("User", userSchema)
+export default User
