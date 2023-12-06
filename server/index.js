00214cd7134680c116fe11c60f5592c7cb21a2ed
nourhandardeer <nourhandardeer3@gmail.com>
2023-12-06 04:34:15 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const mongoose = require('mongoose')
+const User = require('./modules/userModule')
+const Product = require('./modules/productModule')
+const Order = require('./modules/orderModule')
+const bcrypt = require('bcrypt');
 const { notFound, errorHandler } = require("../backend-source-code/Errors");
-const app = express();
 
 
-app.use(notFound);
-app.use(errorHandler);
+const index = express();
 
-app.get("/",(req,res) =>{
+
+
+index.use(notFound);
+index.use(errorHandler);
+
+index.get("/",(req,res) =>{
 res.send("API RUNNING")
 });
-app.listen(5000,console.log("server running"));
+index.listen(5000,console.log("server running"));
