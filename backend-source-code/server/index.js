8d39511d599e9a64aba71b6a9a43fc7f9e27d6a0
Miromah <marmarika56@gmail.com>
2023-12-07 18:21:03 +0200

    .

--- backend-source-code/server/index.js
+++ backend-source-code/server/index.js
@@ -1,42 +1,18 @@
 const express = require("express");
 const mongoose = require('mongoose');
-/* const User = require('./modules/userModule');
-const Product = require('./modules/productModule');
-const Order = require('./modules/orderModule'); */
 const productsRouter = require('./routers/products');
 const usersRoutes = require('./routers/users');
 const bcrypt = require('bcrypt');
-const index = express();
-const app = express()
+require('dotenv/config');
+const app = express();
 const api = process.env.API_URL;
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 //routers
 app.use('${api}/products', productsRouter)
-app.use('${api}/users', usersRoutes)
-/* const { notFound, errorHandler } = require("../backend-source-code/Errors");
+app.use('/users', usersRoutes)
 
-
-const index = express();
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
-
-
-index.use(notFound);
-index.use(errorHandler);
-
-/* app.get('/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-}); */
 mongoose.set("strictQuery", false)
 mongoose
     .connect('mongodb://127.0.0.1:27017/projectData')
--- backend-source-code/server/routers/users.js
+++ backend-source-code/server/routers/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-router.get(`/`, async (req, res) =>{
+router.get('/', async (req, res) =>{
     const userList = await User.find().select('-passwordHash');
 
     if(!userList) {
@@ -12,6 +12,7 @@ router.get(`/`, async (req, res) =>{
     } 
     res.send(userList);
 })
+
 router.get('/:id', async(req,res)=>{
     const user = await User.findById(req.params.id).select('-passwordHash');
 
@@ -24,7 +25,7 @@ router.post('/', async (req,res)=>{
     let user = new User({
         name: req.body.name,
         email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        password: bcrypt.hashSync(req.body.password, 10),
         phone: req.body.phone,
         isAdmin: req.body.isAdmin,
         street: req.body.street,
@@ -40,4 +41,37 @@ router.post('/', async (req,res)=>{
 
     res.send(user);
 })
+router.put('/:id',async (req, res)=> {
+
+    const userExist = await User.findById(req.params.id);
+    let newPassword
+    if(req.body.password) {
+        newPassword = bcrypt.hashSync(req.body.password, 10)
+    } else {
+        newPassword = userExist.password;
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            password: newPassword,
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country,
+        },
+        { new: true}
+    )
+
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+})
+
 module.exports = router;
