a0a16ddb1c5974c7ec8d128cf98df8d9ff38f954
nourr122 <152524706+nourr122@users.noreply.github.com>
2023-12-07 06:53:22 -0800

    Update categories.js

--- backend-source-code/server/routers/categories.js
+++ backend-source-code/server/routers/categories.js
@@ -26,4 +26,17 @@ router.post('/', async (req, res) => {
     
 })
 
+router.delete('/:id', (req, res)=>{
+    //then: return a document
+    Category.findByIdAndRemove(req.params.id).then(category =>{
+        if(category) {
+            return res.status(200).json({success: true, message: 'the category is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "category not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
 module.exports = router;
