98b7029a3765a70374f14c5db7327d7b4d03bd04
nourr122 <152524706+nourr122@users.noreply.github.com>
2023-12-07 08:57:40 -0800

    .

--- backend-source-code/server/index.js
+++ backend-source-code/server/index.js
@@ -1,41 +1,48 @@
 const express = require("express");
 const mongoose = require('mongoose');
-<<<<<<< HEAD
  /*const User = require('./modules/userModule');
 const Product = require('./modules/productModule');
 const Order = require('./modules/orderModule'); */
-=======
->>>>>>> 73729d889c452ec3dbd403ea18c1dab4d2bc436f
 const productsRouter = require('./routers/products');
 const usersRoutes = require('./routers/users');
+const ordersRoutes = require('./routers/orders');
 const bcrypt = require('bcrypt');
-<<<<<<< HEAD
 //const index = express();
 const app = express()
 require('dotenv/config');
 
 
-=======
-require('dotenv/config');
-const app = express();
-const api = process.env.API_URL;
->>>>>>> 73729d889c452ec3dbd403ea18c1dab4d2bc436f
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 //routers
-<<<<<<< HEAD
 const api = process.env.API_URL;
 
-app.use('${api}/products', productsRouter)
-app.use('${api}/users', usersRoutes)
-app.use('${api}/orders', ordersRoutes)
-/* const { notFound, errorHandler } = require("../backend-source-code/Errors");
-=======
 app.use('/products', productsRouter)
 app.use('/users', usersRoutes)
->>>>>>> 73729d889c452ec3dbd403ea18c1dab4d2bc436f
+app.use('/orders', ordersRoutes)
+/* const { notFound, errorHandler } = require("../backend-source-code/Errors");
 
+
+const index = express();
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+
+index.use(notFound);
+index.use(errorHandler);
+
+/* app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}); */
 mongoose.set("strictQuery", false)
 mongoose
     .connect('mongodb://127.0.0.1:27017/projectData')
