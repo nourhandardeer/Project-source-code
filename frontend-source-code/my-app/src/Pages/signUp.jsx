58118233913eae50496597ca628e7f38dedc8be4
elafAhmed <mbarghash666@gmail.com>
2023-12-07 15:12:03 +0200

    .

---
+++ frontend-source-code/my-app/src/Pages/signUp.jsx
@@ -0,0 +1,14 @@
+const signUp = () =>{
+    return(
+        <div>
+            <title>CREATE AN ACCOUNT</title>
+            <input type="name" />
+            <input type="last name" />
+            <input type="user name" />
+            <input type="email" />
+            <input type="password" />
+            <input type="confirm password" />
+        </div>
+    );
+}
+export default signUp;
