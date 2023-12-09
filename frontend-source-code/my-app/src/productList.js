3c63630fbe8c4a6602e188c754021c205a0418ff
nourr122 <152524706+nourr122@users.noreply.github.com>
2023-12-09 02:50:30 -0800

    Update productList.js

--- frontend-source-code/my-app/src/productList.js
+++ frontend-source-code/my-app/src/productList.js
@@ -46,33 +46,7 @@ const ProductList = () => {
 
 
 
-  // Function to fetch products from the backend
-  /* const fetchProducts = async () => {
-    try {
-      const response = await axios.get('/localhost:8000/products');
-      const fetchedProducts = response.data;
-      setProducts(fetchedProducts); // Update state with fetched products
-    } catch (error) {
-      console.error('Error fetching products:', error);
-      // Handle the error as needed (e.g., show an error message to the user)
-    }
-  }; */
 
-  // Fetch products when the component mounts
-
-  /* useEffect(() => {
-    const fetchProductsByCategory = async () => {
-      try {
-        const response = await axios.get(`/products?categories=${categories}`);
-        setProducts(response.data);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-        // Handle error (display error message, etc.)
-      }
-    };
-
-    fetchProductsByCategory();
-  }, [categories]); */
 
   return (
     <div>
