96035b52f531972f126c1113b392d8bbdf3f1d1c
elafAhmed <122807606+elafahmed@users.noreply.github.com>
2023-12-09 14:14:25 +0200

    Update catalogue.js

--- frontend-source-code/my-app/src/catalogue.js
+++ frontend-source-code/my-app/src/catalogue.js
@@ -1,41 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import necklaces from './images/necklaces.png';
 import bracelet from './images/bracelet.png';
 import earrings from './images/earrings.png';
 import rings from './images/rings.jpg';
 
-function catalogue(){
-    return(
-        <div className='cata'>
-            <div className='catalogue1'>
-                
-                <a href="/necklaces">
-                    
-                    <img src={necklaces}>
-                    </img>
-                    <hr color='white'></hr>
-                    <p1> Necklaces</p1>
-                </a>
-                <a href="/rings">
-                    <img src={rings}></img>
-                    <hr color='white'></hr>
-                    <p1>Rings</p1>
-                </a>
-                <a href="/bracelets">
-                <img src={bracelet}></img>
-                <hr color='white'></hr>
-                    <p1> Bracelets</p1>
-                    
-                </a>
-                <a href="/earings">
-                    <img  src={earrings}></img>
-                    <hr color='white'></hr>
-                    <p1> Earring</p1>
-                    
-                </a>
-            </div>
-     
-        </div>
-    );
+function Catalogue() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const history = useHistory();
+
+  const handleSearch = () => {
+    // Redirect to the search results page with the search query
+    history.push(`/search?q=${searchQuery}`);
+  };
+
+  return (
+    <div className='cata'>
+      {/* Search Bar */}
+      <div className="search_box">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+
+      {/* Catalog Items */}
+      <div className='catalogue1'>
+        <a href="/necklaces">
+          <img src={necklaces} alt="Necklaces"></img>
+          <hr color='white'></hr>
+          <p1> Necklaces</p1>
+        </a>
+        <a href="/rings">
+          <img src={rings} alt="Rings"></img>
+          <hr color='white'></hr>
+          <p1> Rings</p1>
+        </a>
+        <a href="/bracelets">
+          <img src={bracelet} alt="Bracelets"></img>
+          <hr color='white'></hr>
+          <p1> Bracelets</p1>
+        </a>
+        <a href="/earrings">
+          <img  src={earrings} alt="Earrings"></img>
+          <hr color='white'></hr>
+          <p1> Earrings</p1>
+        </a>
+      </div>
+    </div>
+  );
 }
-export default catalogue;
+
+export default Catalogue;
+
+
