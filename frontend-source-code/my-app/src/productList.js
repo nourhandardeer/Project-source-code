61b151e1473f931b3fa37736351466e1b3c72b0d
nourhandardeer <nourhandardeer3@gmail.com>
2023-12-09 09:47:09 +0200

    .

--- frontend-source-code/my-app/src/productList.js
+++ frontend-source-code/my-app/src/productList.js
@@ -1,10 +1,10 @@
-// Assuming this is in a React component file (e.g., ProductList.js)
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
   function App() {
  
 const [Products, setProducts] = useState([{}])
 
+<<<<<<< Updated upstream
    useEffect(() => {
     fetch("/products").then (
       res => res.json()
@@ -32,3 +32,56 @@ const [Products, setProducts] = useState([{}])
        
 export default App;
 
+=======
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const categories = '2635,2737'; // Replace with the categories you want to filter
+
+
+
+  // Function to fetch products from the backend
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get('/localhost:8000/products');
+      const fetchedProducts = response.data;
+      setProducts(fetchedProducts); // Update state with fetched products
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      // Handle the error as needed (e.g., show an error message to the user)
+    }
+  };
+
+  // Fetch products when the component mounts
+
+  useEffect(() => {
+    const fetchProductsByCategory = async () => {
+      try {
+        const response = await axios.get(`/products?categories=${categories}`);
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+        // Handle error (display error message, etc.)
+      }
+    };
+
+    fetchProductsByCategory();
+  }, [categories]);
+
+  return (
+    <div>
+      <h2>Product List</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product._id}>
+            <h3>{product.name}</h3>
+            <p>Price: ${product.price}</p>
+            {/* Display other product details as needed */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
+>>>>>>> Stashed changes
